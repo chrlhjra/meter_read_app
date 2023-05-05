547c6b9b31e568d04faf52ac08aa3161ab59172f
chrlhjra <91324862+chrlhjra@users.noreply.github.com>
2023-05-05 14:13:59 +0800

    fsd

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,61 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:camera/camera.dart';
+import 'package:image_picker/image_picker.dart';
 
-Future<void> main() async {
-  // Ensure that plugin services are initialized before `runApp()`
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  // Obtain a list of the available cameras on the device.
-  final cameras = await availableCameras();
-  
-  // Get the first camera from the list.
-  final firstCamera = cameras.first;
-  
-  runApp(MyApp(camera: firstCamera));
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  final CameraDescription camera;
-
-  const MyApp({Key? key, required this.camera}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Camera App',
+      home: CameraScreen(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
-  late File _imageFile;
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({super.key});
+
+  @override
+  State<CameraScreen> createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late final ImagePicker picker;
+
+  File? _imageFile;
 
   @override
   void initState() {
     super.initState();
-    
-    // To display the current output from the camera,
-    // create a CameraController.
-    _controller = CameraController(
-      // Get a specific camera from the list of available cameras.
-      widget.camera,
-      // Define the resolution to use.
-      ResolutionPreset.medium,
-    );
-
-    // Next, initialize the controller. This returns a Future.
-    _initializeControllerFuture = _controller.initialize();
+    picker = ImagePicker();
+    _getImage();
   }
 
-  @override
-  void dispose() {
-    // Dispose of the controller when the widget is disposed.
-    _controller.dispose();
-    super.dispose();
+  Future<void> _getImage() async {
+    final XFile? image = await picker.pickImage(source: ImageSource.camera);
+    if (image != null) {
+      setState(() {
+        _imageFile = File(image.path);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 0, 108, 133),
-          title: const Text('Meter Reader'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            // Take a picture and save it to the temporary directory.
-            try {
-              // Ensure that the camera is initialized.
-              await _initializeControllerFuture;
-
-              // Construct the path where the image should be saved using the
-              // path provider package.
-              final directory = await getTemporaryDirectory();
-              final imagePath = '${directory.path}/${DateTime.now()}.png';
-
-              // Attempt to take a picture and log where it's been saved.
-              await _controller.takePicture();
-
-              // If the picture was taken successfully, set the image file and
-              // update the UI to display the new image.
-              setState(() {
-                _imageFile = File(imagePath);
-              });
-            } catch (e) {
-              // If an error occurs, log the error to the console.
-              print(e);
-            }
-          },
-          child: const Icon(Icons.camera_alt),
-        ),
-        body: Center(
-          // ignore: unnecessary_null_comparison
-          child: _imageFile == null ? const Text('No image taken') : Image.file(_imageFile),
-        ),
-      ),
+    return Scaffold(
+      body: _imageFile == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Center(
+              child: Image.file(_imageFile!),
+            ),
     );
   }
 }
