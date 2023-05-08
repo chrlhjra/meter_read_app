6b9e05a1e738c89137aece757fe4fed5766fb2c5
chrlhjra <91324862+chrlhjra@users.noreply.github.com>
2023-05-08 16:11:39 +0800

    fds

--- lib/camera_screen.dart
+++ lib/camera_screen.dart
@@ -16,10 +16,47 @@ class _CameraScreenState extends State<CameraScreen> {
     final ImagePicker picker = ImagePicker();
     final XFile? image = await picker.pickImage(source: ImageSource.camera);
     if (image != null) {
-      setState(() {
-        _imageFiles.add(File(image.path));
-      });
+      final String? imageName = await _getImageName();
+      if (imageName != null && imageName.isNotEmpty) {
+        final String imagePath = '${image.path.split('/').last}';
+        final File newImage = await File(image.path).copy('$imagePath/$imageName');
+        setState(() {
+          _imageFiles.add(newImage);
+        });
+      }
     }
+  }
+
+  Future<String?> _getImageName() async {
+    final nameController = TextEditingController();
+    return showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Image Name'),
+          content: TextFormField(
+            controller: nameController,
+            decoration: const InputDecoration(hintText: 'Enter name'),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a name';
+              }
+              return null;
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, nameController.text),
+              child: const Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -34,26 +71,34 @@ class _CameraScreenState extends State<CameraScreen> {
           : ListView.builder(
               itemCount: _imageFiles.length,
               itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => Scaffold(
-                          appBar: AppBar(
-                            title: Text(_imageFiles[index].path),
-                            backgroundColor:
-                                const Color.fromRGBO(0, 108, 133, 1),
-                          ),
-                          body: Center(
-                            child: Image.file(_imageFiles[index]),
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  color: Colors.orangeAccent,
+                  child: ListTile(
+                    leading: Image.file(
+                      _imageFiles[index],
+                      width: 50,
+                      height: 50,
+                    ),
+                    title: Text(_imageFiles[index].path.split('/').last),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Scaffold(
+                            appBar: AppBar(
+                              title: Text(_imageFiles[index].path.split('/').last),
+                            ),
+                            body: Center(
+                              child: Image.file(
+                                _imageFiles[index],
+                                fit: BoxFit.cover,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  },
-                  child: ListTile(
-                    title: Text(_imageFiles[index].path.split('/').last),
+                      );
+                    },
                   ),
                 );
               },
